5892a8cc4af0687c2dd5a5baca373f71148db018
AmandeepPooni <pooni_amandeep@ahoo.com>
2023-03-31 23:05:32 +0530

    Remove update comment functionality

--- functions/src/routes/comments/index.ts
+++ functions/src/routes/comments/index.ts
@@ -1,12 +1,10 @@
 import {Router} from 'express'
 import createComment from './createComment'
-import updateComment from './updateComment'
 import deleteComment from './deleteComment'
 
 var router = Router()
 
 router.post('/', createComment)
-router.put('/:id', updateComment)
 router.delete('/:id', deleteComment)
 
 export default router
